c9e485ca3d753314c83d5a0348c832bdcf51ee3a
Laura Evans <lauraevans@Lauras-MacBook-Pro-2.local>
2016-11-23 18:38:33 +0200

    add jsonParser functino not using SwiftyJSON to show comparison

--- jsonSwifty/jsonSwifty/ViewController.swift
+++ jsonSwifty/jsonSwifty/ViewController.swift
@@ -18,11 +18,14 @@ class TableViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        parseJSON()
+        //parseJSONWithSwiftyJSON()
+        parseJSONTheNotSoFunWay()
     }
 
     //MARK: Methods
-    func parseJSON() {
+    
+    // parse data with Swifty JSON :)
+    func parseJSONWithSwiftyJSON() {
         
         // get file and apply it to a path
         let path = Bundle.main.path(forResource: "jsonFile", ofType: "json")
@@ -47,6 +50,52 @@ class TableViewController: UITableViewController {
         self.tableView.reloadData()
         print(names)
     }
+    
+    // parse data without Swifty JSON :(
+    func parseJSONTheNotSoFunWay() {
+        
+        // get file and apply it to a path
+        let path = Bundle.main.path(forResource: "jsonFile", ofType: "json")
+        
+        //Grab json data and apply to to NSData
+        let jsonData = NSData(contentsOfFile: path!)
+        
+        do {
+            let jsonObject = try JSONSerialization.jsonObject(with: jsonData! as Data, options: .allowFragments)
+            if let json = jsonObject as? [String: AnyObject] {
+                
+                print("Printing JSON")
+                print(json)
+                
+                guard let people = json["People"] as? [String: AnyObject] else {
+                    print("could not parse people")
+                    return
+                }
+                
+                print("Printing Number of People")
+                print(people.count)
+                
+                for i in 1...people.count {
+                    
+                    guard let person = people["Person\(i)"] as? [String: AnyObject] else {
+                        print("could not parse person")
+                        return
+                    }
+                    
+                    names.append(person["Name"] as! String)
+                    self.tableView.reloadData()
+                    print(names)
+                    
+                }
+                
+            }
+        } catch {
+            // Handle Error
+        }
+    }
+
+    
+    
     
     //MARK: Tableview methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
